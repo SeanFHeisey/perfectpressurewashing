0ed246b450a42356c1f5d20765f2df54f2d482c5
Sean <seaner2@frontier.com>
2024-10-03 20:17:46 -0400

    backend test

---
+++ js/backend.js
@@ -0,0 +1,19 @@
+function changeHeaderIcons() {
+    const iconBookmark = document.getElementById('bookmark');
+    const iconMail = document.getElementById('mail');
+    const iconPhone = document.getElementById('phone');
+    if (iconBookmark) {
+        console.log('gooda');
+    }
+    if (iconMail) {
+        console.log('goodb');
+    }
+    if (iconPhone) {
+        console.log('goodc');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    changeHeaderIcons();
+});
+
