fb1876eee89de35369b84343dae2f8394e6b4407
Sean <seaner2@frontier.com>
2024-10-25 15:47:09 -0400

    updating scroll function

--- js/backend.js
+++ js/backend.js
@@ -2,10 +2,11 @@ function topHeader() {
     const headerTop = document.getElementById('top-header');
     const headerMain = document.getElementById('main-header');
     const scrollPosition = window.scrollY;
-    headerTop.style.top = `${Math.max(0, -scrollPosition)}px`; // Top header scrolls out
+    headerTop.style.top = `${Math.max(50 - scrollPosition, -50)}px`; // Top header scrolls out
     headerMain.style.top = `${Math.max(50 - scrollPosition, 0)}px`; // Main header adjusts as well
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    topHeader(); // ensure header initial position is 0px
     window.addEventListener('scroll', topHeader);
 });
